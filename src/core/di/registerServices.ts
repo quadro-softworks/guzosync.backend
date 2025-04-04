28cb1bf25dd84c6b3a8e325502ab834e458299a1
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-04-04 23:53:40 +0300

    Add controllers and handlers for Logout, Request Password Reset, Confirm Password Reset, and Get My Profile features

--- src/core/di/registerServices.ts
+++ src/core/di/registerServices.ts
@@ -1,3 +1,7 @@
+import {
+  IEmailServiceMeta,
+  NodemailerEmailService,
+} from '@core/services/email.service';
 import { RegisterUserHandler } from './../../modules/userManagement/features/register-user/register-user.handler';
 import { IEventBus, IEventBusMeta, AppEventBus } from '@core/events/event-bus';
 import {
@@ -11,15 +15,53 @@ import {
   JwtService,
 } from '@core/services/jwt.service';
 import { DependencyContainer } from 'tsyringe';
+import { ConfirmPasswordResetHandler } from '@modules/userManagement/features/confirm-password-reset/confirm-password-reset.handler';
+import { ConfirmPasswordResetController } from '@modules/userManagement/features/confirm-password-reset/confirm-password-reset.controller';
+import { RequestPasswordResetHandler } from '@modules/userManagement/features/request-password-reset/request-password-reset.handler';
+import { RequestPasswordResetController } from '@modules/userManagement/features/request-password-reset/request-password-reset.controller';
+import { LogoutUserController } from '@modules/userManagement/features/logout-user/logout-user.controller';
+import { GetMyProfileController } from '@modules/userManagement/features/get-my-profile/get-my-profile.controller';
+import { GetMyProfileHandler } from '@modules/userManagement/features/get-my-profile/get-my-profile.handler';
 
 export default function registerServices(appContainer: DependencyContainer) {
   console.log('Registering dependencies...'); // Add log for debugging
+
   appContainer.registerSingleton(IEventBusMeta.name, AppEventBus);
+
   appContainer.registerSingleton(
     IHashingServiceMeta.name,
     BcryptHashingService,
   );
+
   appContainer.registerSingleton(IJwtServiceMeta.name, JwtService);
+
+  appContainer.registerSingleton(
+    IEmailServiceMeta.name,
+    NodemailerEmailService,
+  );
+
+  appContainer.register('LogoutUserController', LogoutUserController);
+
+  appContainer.register(
+    'RequestPasswordResetHandler',
+    RequestPasswordResetHandler,
+  );
+  appContainer.register(
+    'RequestPasswordResetController',
+    RequestPasswordResetController,
+  );
+
+  appContainer.register(
+    'ConfirmPasswordResetHandler',
+    ConfirmPasswordResetHandler,
+  );
+  appContainer.register(
+    'ConfirmPasswordResetController',
+    ConfirmPasswordResetController,
+  );
+
+  appContainer.register('GetMyProfileHandler', GetMyProfileHandler);
+  appContainer.register('GetMyProfileController', GetMyProfileController);
 
   // appContainer.register('RegisterUserHandler', RegisterUserHandler);
   console.log('Registered dependencies!'); // Add log for debugging
