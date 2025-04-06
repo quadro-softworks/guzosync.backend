496d77ea39da681c58704ec60a949264446cfbf5
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-04-06 23:06:03 +0300

    Fix AlreadyExistsError status code and message for better clarity

--- src/core/errors/already-exists.error.ts
+++ src/core/errors/already-exists.error.ts
@@ -2,8 +2,8 @@
 import { ApiError } from './api-error';
 
 export class AlreadyExistsError extends ApiError {
-  constructor(message: string = 'Bad Request') {
-    super(400, message);
+  constructor(message: string = 'Already Exists') {
+    super(409, message);
     Object.setPrototypeOf(this, AlreadyExistsError.prototype);
   }
 }
