fcc5c5eb9435c5bf1925c2210a6cf65288ec7c63
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-04-09 17:19:52 +0300

    refactor: update type imports to use mongoose.mongo.BSON.ObjectId for consistency

--- src/core/domain/valueObjects/index.ts
+++ src/core/domain/valueObjects/index.ts
@@ -1,18 +1,19 @@
-import { Schema } from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
-export type BusStopId = Schema.Types.ObjectId;
-export type BusId = Schema.Types.ObjectId;
-export type IncidentId = Schema.Types.ObjectId;
-export type FeedbackId = Schema.Types.ObjectId;
-export type RouteId = Schema.Types.ObjectId;
-export type TripId = Schema.Types.ObjectId;
-export type ScheduleId = Schema.Types.ObjectId;
-export type NotificationId = Schema.Types.ObjectId;
-export type AlertId = Schema.Types.ObjectId;
+export type BusStopId = mongoose.mongo.BSON.ObjectId;
+export type BusId = mongoose.mongo.BSON.ObjectId;
+export type IncidentId = mongoose.mongo.BSON.ObjectId;
+export type FeedbackId = mongoose.mongo.BSON.ObjectId;
+export type RouteId = mongoose.mongo.BSON.ObjectId;
+export type TripId = mongoose.mongo.BSON.ObjectId;
+export type ScheduleId = mongoose.mongo.BSON.ObjectId;
+export type NotificationId = mongoose.mongo.BSON.ObjectId;
+export type AlertId = mongoose.mongo.BSON.ObjectId;
 
 // User Ids
-export type UserId = Schema.Types.ObjectId;
-export type QueueRegulatorId = Schema.Types.ObjectId;
-export type ControlCenterAdminId = Schema.Types.ObjectId;
-export type BusDriverId = Schema.Types.ObjectId;
-export type PassengerId = Schema.Types.ObjectId;
+export type UserId = mongoose.mongo.BSON.ObjectId;
+export type NotificationSettingsId = mongoose.mongo.BSON.ObjectId;
+export type QueueRegulatorId = mongoose.mongo.BSON.ObjectId;
+export type ControlCenterAdminId = mongoose.mongo.BSON.ObjectId;
+export type BusDriverId = mongoose.mongo.BSON.ObjectId;
+export type PassengerId = mongoose.mongo.BSON.ObjectId;
