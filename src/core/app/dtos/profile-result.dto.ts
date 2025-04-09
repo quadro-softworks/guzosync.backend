3a3b31eacc55830f4f45b50b33c9c88753951052
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-04-09 17:48:47 +0300

    feat: refactor ProfileResult to interface and class for improved structure and type safety

--- src/core/app/dtos/profile-result.dto.ts
+++ src/core/app/dtos/profile-result.dto.ts
@@ -1,7 +1,35 @@
-import { User } from '@core/domain/models/user.model';
+import { IUserResult } from '@core/app/dtos/user-result.dto';
+import { Role } from '@core/domain/enums/role.enum';
+import { IUser, User } from '@core/domain/models/user.model';
+import { UserId } from '@core/domain/valueObjects';
 
 // Public representation excluding sensitive data
-export type ProfileResult = Omit<
-  User,
-  'password' | 'passwordResetToken' | 'passwordResetExpires'
->;
+export interface IProfileResult
+  extends Omit<
+    User,
+    'password' | 'passwordResetToken' | 'passwordResetExpires'
+  > {}
+
+export class ProfileResult {
+  id: UserId;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: Role;
+  phoneNumber: string;
+  profileImage?: string;
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor(user: IUserResult) {
+    this.id = user.id;
+    this.firstName = user.firstName;
+    this.lastName = user.lastName;
+    this.email = user.email;
+    this.role = user.role;
+    this.phoneNumber = user.phoneNumber;
+    this.profileImage = user.profileImage;
+    this.createdAt = user.createdAt;
+    this.updatedAt = user.updatedAt;
+  }
+}
